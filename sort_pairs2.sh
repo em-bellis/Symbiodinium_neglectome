ac4953de4501a8930cb69b70508215e4a26d2a22
Emily Bellis <emilysbellis@gmail.com>
2016-11-17 21:51:20 -0800

    Create sort_pairs2.sh
    Shell script for sorting filtered reads into R1, R2, and single-end reads for which one was removed during quality filtering.

---
+++ sort_pairs2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#first argument: barcode
+#usage
+
+cat R1.hq.fastq R2.hq.fastq > cat.fastq
+
+merge.pl cat.fastq merged.txt
+sort merged.txt -k1,1d -k2,2n >sort.txt
+rm -rf merged.txt
+
+Sep_Pairs2.pl sort.txt
+Sep_FandR.pl pairs.txt
+
+unmerge.pl singles.R1.txt R1_hq.fastq
+unmerge.pl singles.R2.txt R2_hq.fastq
+unmerge.pl singles.txt singles.fastq
+
+rm -rf pairs.txt
+rm -rf singles.txt
+rm -rf singles.R1.txt
+rm -rf singles.R2.txt
+rm -rf sort.txt
+rm -rf cat.fastq
